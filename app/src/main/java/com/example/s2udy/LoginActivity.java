68340ce7be9d5351a91fc1da2962556c66933c18
Nicole Liang <nicolenliang@gmail.com>
2021-07-19 18:59:19 -0400

    login toast message include logged in username

--- app/src/main/java/com/example/s2udy/LoginActivity.java
+++ app/src/main/java/com/example/s2udy/LoginActivity.java
@@ -71,7 +71,7 @@ public class LoginActivity extends AppCompatActivity
                     Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Toast.makeText(LoginActivity.this, "login successful!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "login " + user.getUsername() + " successful!", Toast.LENGTH_SHORT).show();
                 goMainActivity();
             }
         });
