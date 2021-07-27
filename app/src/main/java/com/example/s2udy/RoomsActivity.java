0bc0c3a67dca1212227204ad7f6d93f4e43a046c
Nicole Liang <nicolenliang@gmail.com>
2021-07-27 01:44:15 -0400

    attach endless scroll listener to recyclerview

--- app/src/main/java/com/example/s2udy/RoomsActivity.java
+++ app/src/main/java/com/example/s2udy/RoomsActivity.java
@@ -85,6 +85,7 @@ public class RoomsActivity extends AppCompatActivity
                 moreRooms();
             }
         };
+        rvRooms.addOnScrollListener(scrollListener);
     }
 
     private void moreRooms()
