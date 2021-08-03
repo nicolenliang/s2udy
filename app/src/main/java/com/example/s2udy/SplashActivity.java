3e3c2db23b7491a52b8f76ab949dbbe09dea4ff8
Nicole Liang <nicolenliang@gmail.com>
2021-08-02 20:48:13 -0400

    splash screen on app open

---
+++ app/src/main/java/com/example/s2udy/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.s2udy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
