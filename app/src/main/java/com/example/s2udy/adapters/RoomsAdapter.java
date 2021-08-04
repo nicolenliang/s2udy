984dd53dfb9d1bee782b07bbedc6657ff6d6d8bf
Nicole Liang <nicolenliang@gmail.com>
2021-08-04 18:52:49 -0400

    implemented private rooms with passcode

--- app/src/main/java/com/example/s2udy/adapters/RoomsAdapter.java
+++ app/src/main/java/com/example/s2udy/adapters/RoomsAdapter.java
@@ -1,12 +1,17 @@
 package com.example.s2udy.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,11 +29,18 @@ public class RoomsAdapter extends RecyclerView.Adapter
     public static final String TAG = "RoomsAdapter";
     Context context;
     List<Room> rooms;
+    onLongClickListener longClickListener;
 
-    public RoomsAdapter(Context context, List<Room> rooms)
+    public interface onLongClickListener
+    {
+        void onItemLongClicked(int position);
+    }
+
+    public RoomsAdapter(Context context, List<Room> rooms, onLongClickListener longClickListener)
     {
         this.context = context;
         this.rooms = rooms;
+        this.longClickListener = longClickListener;
     }
 
     @NonNull
@@ -61,6 +73,8 @@ public class RoomsAdapter extends RecyclerView.Adapter
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
     {
         TextView tvRoomName, tvHost, tvChatEnable, tvTags, tvDescription;
+        ImageView ivLocked;
+        EditText etPasscode;
 
         public ViewHolder(@NonNull View itemView)
         {
@@ -70,8 +84,18 @@ public class RoomsAdapter extends RecyclerView.Adapter
             tvChatEnable = itemView.findViewById(R.id.tvChatEnable);
             tvTags = itemView.findViewById(R.id.tvTags);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            ivLocked = itemView.findViewById(R.id.ivLocked);
 
             itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return false;
+                }
+            });
         }
 
         public void bind(Room room)
@@ -86,6 +110,8 @@ public class RoomsAdapter extends RecyclerView.Adapter
             tags = tags.substring(1, tags.length() - 1);
             tvTags.setText("tags: " + tags);
             tvDescription.setText(room.getDescription());
+            if (!(room.getPasscode() == null))
+                ivLocked.setVisibility(View.VISIBLE);
         }
 
         // click to navigate to in-room screen
@@ -98,9 +124,44 @@ public class RoomsAdapter extends RecyclerView.Adapter
             {
                 Room room = rooms.get(position);
                 Log.i(TAG,"onClick room: " + room.getName());
+
                 Intent i = new Intent(context, InRoomActivity.class);
                 i.putExtra(Room.class.getSimpleName(), Parcels.wrap(room));
-                context.startActivity(i);
+
+                if (room.getPasscode() == null)
+                    context.startActivity(i);
+                else
+                {
+                    View editDialog = LayoutInflater.from(context).inflate(R.layout.dialog_edit, null);
+                    etPasscode = editDialog.findViewById(R.id.etDialog);
+
+                    AlertDialog dialog = new AlertDialog.Builder(context)
+                            .setTitle("enter passcode")
+                            .setMessage("room is private! please enter a passcode")
+                            .setView(editDialog)
+                            .setPositiveButton("submit", new DialogInterface.OnClickListener()
+                            {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which)
+                                {
+                                    String passcode = etPasscode.getText().toString();
+                                    if (passcode.equals(room.getPasscode()))
+                                        context.startActivity(i);
+                                    else
+                                        Toast.makeText(context, "passcode incorrect!", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .setNegativeButton("cancel", new DialogInterface.OnClickListener()
+                            {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which)
+                                {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .create();
+                    dialog.show();
+                }
             }
         }
     }
